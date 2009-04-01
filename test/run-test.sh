5491115c11cc3c6a0073a75ef41442dc1f927948
Benjamin Berg <benjamin@sipsolutions.net>
2009-04-01 20:43:32 +0200

    Fix the test script.

---
+++ test/run-test.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+# Stop if anything goes wrong.
+set -e
+
+# Remove any old test_project that may exist
+rm -rf ./test_project
+
+# Setup the test_project, using the data in "data"
+../sdaps.py ./test_project setup data/debug.odt data/debug.pdf data/debug.internetquestions
+
+# Create a cover page in ./test_project/cover.pdf
+../sdaps.py ./test_project cover
+
+# Create 10 unique sheets that can be printed and handed out
+../sdaps.py ./test_project stamp 10
+
+# Dumps a list of all the questionaire IDs (ie. the ideas of each of the 100 sheets)
+../sdaps.py ./test_project ids
+
+# Import the scanned data. The data has to be a multipage 1bpp tif file.
+../sdaps.py ./test_project add data/debug.tif
+
+# Analyse the image data
+../sdaps.py ./test_project recognize
+
+# And finally, create a report with the result
+../sdaps.py ./test_project report
+
