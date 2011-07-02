2296b3f86b2a2906c9eb6eba17774e88dcf900fc
Benjamin Berg <benjamin@sipsolutions.net>
2011-07-02 11:50:46 +0200

    Add --no-copy and --copy options to "add" command.

--- sdaps/add.py
+++ sdaps/add.py
@@ -20,26 +20,42 @@ import os
 
 import model
 import script
+import optparse
 
 from ugettext import ugettext, ungettext
 _ = ugettext
 
-
-@script.register
-@script.logfile
-@script.doc(_(u'''files
+usage=_("""[options] files
 
 	Add scanned questionnaires to the survey.
 
-	files: TIFF-Images containing scanned questionnaires.
-	'''))
-def add (survey_dir, *files):
+	file: TIFF-Images containing scanned questionnaires.""")
+
+# Stupid bugger always adds a "Usage:" string that we do not want.
+parser = optparse.OptionParser(usage=optparse.SUPPRESS_USAGE)
+
+parser.set_defaults(print_survey_id=True)
+parser.set_defaults(print_questionnaire_id=True)
+
+parser.add_option('--copy', action="store_const",
+                  help=_('Copy the TIFF into the project directory (default).'),
+                  dest='copy', const=True)
+parser.add_option('--no-copy', action="store_const",
+                  help=_('Do not copy the TIFF. Instead reference it with a relative path.'),
+                  dest='copy', const=False)
+
+@script.register
+@script.logfile
+@script.doc(usage + '\n\n\t' + '\n\t'.join(parser.format_help().split('\n')))
+def add (survey_dir, *args):
 	import image
 	import subprocess
 	import sys
 	import shutil
 
 	survey = model.survey.Survey.load(survey_dir)
+
+	(options, files) = parser.parse_args(list(args))
 
 	for file in files :
 
@@ -48,15 +64,21 @@ def add (survey_dir, *files):
 		if not image.check_tiff_monochrome(file):
 			print _('Invalid input file %s. You need to specify a (multipage) monochrome TIFF as input.' % file);
 
-		tiff = survey.new_path('%i.tif')
-		shutil.copyfile(file, tiff)
+		if options.copy:
+			tiff = survey.new_path('%i.tif')
+			shutil.copyfile(file, tiff)
+		else:
+			tiff = file
 
 		num_pages = image.get_tiff_page_count(tiff)
 
 		c = survey.questionnaire.page_count
 		assert num_pages % c == 0
 
-		tiff = os.path.basename(tiff)
+		if options.copy:
+			tiff = os.path.basename(tiff)
+		else:
+			tiff = os.path.relpath(os.path.abspath(tiff), survey.survey_dir)
 
 		for i in range(num_pages / c) :
 			sheet = model.sheet.Sheet()
