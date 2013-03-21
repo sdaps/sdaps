sdaps/sdaps

commit 4241db27e433a6c1b79bd0e207f5ecec33a3132b

Robustness fixes in the classic style recognition.

sdaps/setup/setup.py
@@ -100,6 +100,7 @@ def setup(survey, cmdline):
 
     # Last but not least calculate the survey id
     survey.calculate_survey_id()
+    survey.survey_id = 1591158456
 
     if not survey.check_settings():
         log.error(_("Some combination of options and project properties do not work. Aborted Setup."))

sdaps/recognize/classic.py
@@ -37,21 +37,25 @@ _ = ugettext
 def get_page_rotation(image):
     # Returns page rotation or "None" if it cannot be retrieved
 
-    rotated, page_number = get_pagenumber_and_rotation(image)
+    res = get_pagenumber_and_rotation(image)
+    if res is None:
+        return None
 
-    return rotated
+    return res[0]
 
 
 def get_page_number(image):
     # Returns page number or "None" if it cannot be retrieved
     # Returns page number rotation or "None" if it cannot be retrieved
 
-    rotated, page_number = get_pagenumber_and_rotation(image)
-
-    if rotated:
-        # This may never happen
+    res = get_pagenumber_and_rotation(image)
+    if res is None:
+        return None
+    # The page may not be rotated at this point
+    if res[0]:
         raise RecognitionError
-    return page_number
+
+    return res[1]
 
 
 def get_survey_id(image):

commit 3b811517d6179297089f62a671f8a4996672f121

ODT mode always had the same survey ID.
This was a regression introduced in commit 4241db27. The line made
it into the commit, even though it was only there for testing purposes.

sdaps/setup/setup.py
@@ -100,7 +100,6 @@ def setup(survey, cmdline):
 
     # Last but not least calculate the survey id
     survey.calculate_survey_id()
-    survey.survey_id = 1591158456
 
     if not survey.check_settings():
         log.error(_("Some combination of options and project properties do not work. Aborted Setup."))