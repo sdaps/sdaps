2885dd19ba7bf9c0d03cce4cd23be84e28c8639e
Benjamin Berg <benjamin@sipsolutions.net>
2013-04-02 03:01:35 +0200

    Try a manual segementation based solely on the x-axis overlap.

--- sdaps/gamera/test.py
+++ sdaps/gamera/test.py
@@ -35,16 +35,43 @@ cknn = knn.kNNInteractive([],
    0)
 cknn.from_xml_filename(orig_argv[4])
 
-classified = cknn.group_and_update_list_automatic(ccs,
-    grouping_function=classify.ShapedGroupingFunction(0),
-    max_parts_per_group=1)
+#classified = cknn.group_and_update_list_automatic(ccs,
+#    #grouping_function=classify.BoundingBoxGroupingFunction(15),
+#    grouping_function=classify.ShapedGroupingFunction(15),
+#    max_parts_per_group=4)
+
+ccs.sort(key=lambda img: img.offset_x)
+i = 0
+while i < len(ccs) - 1:
+    if ccs[i].offset_x + ccs[i].ncols >= ccs[i+1].offset_x:
+        # We can merge the two
+        if not isinstance(ccs[i], core.MlCc):
+            ccs[i] = core.MlCc(img, ccs[i].label, ccs[i])
+
+        ccs[i].add_label(ccs[i+1].label, ccs[i+1])
+        ccs.pop(i+1)
+
+    else:
+        i += 1
+
+added, removed = cknn.classify_list_automatic(ccs)
+print added, removed
+classified = ccs
 
 classified.sort(key=lambda img: img.offset_x)
+
 
 for c in classified:
     print c.id_name
 
+# Merge the groups display
+for i in xrange(len(classified)):
+    if isinstance(classified[i], core.MlCc):
+        classified[i] = classified[i].convert_to_cc()
+
 colored = img.graph_color_ccs(classified, None, 1)
+#colored = img.color_ccs()
+
 surface = convert.to_surface(colored)
 
 debug.show_surface(surface)
