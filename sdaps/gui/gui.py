sdaps/sdaps

commit c880eb8d364bcc102619752f91b4245eae2fef1c

gui: Exit the mainloop if Ctrl+C is pressed in the terminal
Someone just asked in #gtk+ how this works. Looking into it a bit
there is a nice solution using the GLib mainloop.

sdaps/gui/gui.py
@@ -17,11 +17,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from gi.repository import GObject
+from gi.repository import GLib
 from gi.repository import Gtk
 from gi.repository import Gdk
 import os
 import time
 import sys
+import signal
 
 from sdaps import model
 from sdaps import surface
@@ -47,6 +49,10 @@ def gui(survey, cmdline):
     if not provider.images:
         log.error(_("The survey does not have any images! Please add images (and run recognize) before using the GUI."))
         return 1
+
+    # Exit the mainloop if Ctrl+C is pressed in the terminal.
+    GLib.unix_signal_add_full(GLib.PRIORITY_HIGH, signal.SIGINT, lambda *args : Gtk.main_quit(), None)
+
     MainWindow(provider).run()
 
 

commit b5f2ff47d3295bd4c2c78eaee8c4750b2efe252c

If unix_signal_add_full cannot be found, just ignore it.
This is a bug introduced in c880eb8d364. Just add a try/except block
to catch the error (removing it would also work).

sdaps/gui/gui.py
@@ -51,8 +51,12 @@ def gui(survey, cmdline):
         log.error(_("The survey does not have any images! Please add images (and run recognize) before using the GUI."))
         return 1
 
-    # Exit the mainloop if Ctrl+C is pressed in the terminal.
-    GLib.unix_signal_add_full(GLib.PRIORITY_HIGH, signal.SIGINT, lambda *args : Gtk.main_quit(), None)
+    try:
+        # Exit the mainloop if Ctrl+C is pressed in the terminal.
+        GLib.unix_signal_add_full(GLib.PRIORITY_HIGH, signal.SIGINT, lambda *args : Gtk.main_quit(), None)
+    except AttributeError:
+        # Whatever, it is only to enable Ctrl+C anyways
+        pass
 
     MainWindow(provider).run()
 