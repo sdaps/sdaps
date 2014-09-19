c1297892fc0ba9878eca4eafcc6e73f1b4019a33
ccollins <chuck.collins@gmail.com>
2014-09-19 12:53:16 -0500

    copyright/license info

--- tex/qrcode.py
+++ tex/qrcode.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
-
+# SDAPS - Scripts for data acquisition with paper based surveys
+# Copyright(C) 2014, Michael Batchelor <batchelor@nationbuilder.com>
+# Copyright(C) 2014, Chuck Collins <chuck@nationbuilder.com>
+# Copyright(C) 2014, Jacob Green <jacob@nationbuilder.com>
+# Copyright(C) 2014, Dustin Ngo <dustin@nationbuilder.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from reportlab.graphics.barcode import qr
 from reportlab.graphics.shapes import Drawing
 from reportlab.lib.pagesizes import letter
