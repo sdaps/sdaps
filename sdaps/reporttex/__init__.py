sdaps/sdaps

commit 0d875c8d80dfd2eaf3ee124a7688385137ffcf66

Properly define coding of python files as utf-8
The python files had a coding of utf8. This seemed to work fine
however xgettext would stumble over it. This commit also reverts
154bcdad82 wich is not needed with this fix.

sdaps/reporttex/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 # SDAPS - Scripts for data acquisition with paper based surveys
 # Copyright(C) 2008, Christoph Simon <post@christoph-simon.eu>
 # Copyright(C) 2008, 2011, Benjamin Berg <benjamin@sipsolutions.net>
@@ -120,7 +120,7 @@ def report(survey, filter, filename=None, papersize=None, small=0, suppress=None
     \usepackage{ifxetex}
     \ifxetex
     \else
-      \usepackage[utf8]{inputenc}
+      \usepackage[utf-8]{inputenc}
     \fi
     \usepackage[%(language)s]{babel}
 

sdaps/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 # SDAPS - Scripts for data acquisition with paper based surveys
 # Copyright(C) 2008, Christoph Simon <post@christoph-simon.eu>
 # Copyright(C) 2008, Benjamin Berg <benjamin@sipsolutions.net>

sdaps/add/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 # SDAPS - Scripts for data acquisition with paper based surveys
 # Copyright(C) 2008, Christoph Simon <post@christoph-simon.eu>
 # Copyright(C) 2008, Benjamin Berg <benjamin@sipsolutions.net>

sdaps/annotate/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 # SDAPS - Scripts for data acquisition with paper based surveys
 # Copyright(C) 2012, Benjamin Berg <benjamin@sipsolutions.net>
 #

sdaps/boxgallery/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 # SDAPS - Scripts for data acquisition with paper based surveys
 # Copyright(C) 2008, Christoph Simon <post@christoph-simon.eu>
 # Copyright(C) 2008, Benjamin Berg <benjamin@sipsolutions.net>

sdaps/convert/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 # SDAPS - Scripts for data acquisition with paper based surveys
 # Copyright(C) 2013, Benjamin Berg <benjamin@sipsolutions.net>
 #

sdaps/cover/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 # SDAPS - Scripts for data acquisition with paper based surveys
 # Copyright(C) 2008, Christoph Simon <post@christoph-simon.eu>
 # Copyright(C) 2008, Benjamin Berg <benjamin@sipsolutions.net>

sdaps/csvdata/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 # SDAPS - Scripts for data acquisition with paper based surveys
 # Copyright(C) 2008, Christoph Simon <post@christoph-simon.eu>
 # Copyright(C) 2008, Benjamin Berg <benjamin@sipsolutions.net>

commit 9b27f2387fe02b1aa4afe20127dbe94062d4d474

Fix input encoding in report generation.
This was broken in 0d875c8d8.

sdaps/reporttex/__init__.py
@@ -120,7 +120,7 @@ def report(survey, filter, filename=None, papersize=None, small=0, suppress=None
     \usepackage{ifxetex}
     \ifxetex
     \else
-      \usepackage[utf-8]{inputenc}
+      \usepackage[utf8]{inputenc}
     \fi
     \usepackage[%(language)s]{babel}
 