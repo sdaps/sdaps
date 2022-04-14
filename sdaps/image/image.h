8e9d6fea86a7d7db37de07fafc8d005b54df5f75
Benjamin Berg <benjamin@sipsolutions.net>
2022-04-15 01:28:35 +0200

    image: Keep full double precision for coverage values
    Returning/casting to single precision floating point is kind of
    pointless.

--- sdaps/image/image.h
+++ sdaps/image/image.h
@@ -65,10 +65,10 @@ gboolean
 find_box_corners(cairo_surface_t *surface, cairo_matrix_t *matrix, gdouble mm_x, gdouble mm_y, gdouble mm_width, gdouble mm_height,
                  gdouble *mm_x1, gdouble *mm_y1, gdouble *mm_x2, gdouble *mm_y2, gdouble *mm_x3, gdouble *mm_y3, gdouble *mm_x4, gdouble *mm_y4);
 
-float
+gdouble
 get_coverage(cairo_surface_t *surface, cairo_matrix_t *matrix, gdouble mm_x, gdouble mm_y, gdouble mm_width, gdouble mm_height);
 
-float
+gdouble
 get_masked_coverage(cairo_surface_t *surface, cairo_surface_t *mask, gint x, gint y);
 
 gdouble
