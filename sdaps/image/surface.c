sdaps/sdaps

commit 2c32e99755f1d098ff8968ad4362f234479a1d49

image: Fix pixel counting masks for big endian
The mask calculation was wrong for big endian. Make the code a bit less
obscure and fix the big endian mask generation at the same time. This
also removes the useless extra loop iteration in case the end fell on a
32bit boundary.

sdaps/image/surface.c
@@ -237,14 +237,14 @@ count_black_pixel_unchecked(guint32* pixels, guint32 stride, gint x, gint y, gin
 		int pos;
 
 #if G_BYTE_ORDER == G_BIG_ENDIAN
-		start_mask = (1 << (x & 0x1f)) - 1;
-		end_mask = -(1 << ((x + width) & 0x1f));
+		start_mask = 0xffffffff >> (x & 0x1f);
+		end_mask = (0xffffffff << (-(x + width) & 0x1f));
 #else
-		start_mask = -(1 << (x & 0x1f));
-		end_mask = (1 << ((x + width) & 0x1f)) - 1;
+		start_mask = 0xffffffff << (x & 0x1f);
+		end_mask = (0xffffffff >> (-(x + width) & 0x1f));
 #endif
 		start = x >> 5;
-		end = (x + width) >> 5;
+		end = (x + width - 1) >> 5;
 
 		if (start == end) {
 			black_pixel += WORD_COUNT_BITS(pixels[start + y_pos * stride / 4] & start_mask & end_mask);
@@ -314,11 +314,11 @@ count_black_pixel_masked_unchecked(guint32* pixels, guint32 stride, guint32 *mas
 		int pos;
 
 #if G_BYTE_ORDER == G_BIG_ENDIAN
-		end_mask = -(1 << (width & 0x1f));
+		end_mask = (0xffffffff << (-(x + width) & 0x1f));
 #else
-		end_mask = (1 << (width & 0x1f)) - 1;
+		end_mask = (0xffffffff >> (-(x + width) & 0x1f));
 #endif
-		end = width >> 5;
+		end = (width - 1) >> 5;
 
 		for (pos = 0; pos <= end; pos++) {
 			/* Note that a shift of 32 is not defined, it may also be 0. */

commit 42a6bf383cad12b84deeb9feee23cdc8ca19e975

image: Fix end bitmask for masked pixel counting
Unfortunately, commit git show 2c32e99755f1 ("image: Fix pixel counting
masks for big endian") introduced a regression as it copied the end mask
calculation from the unmasked function, incorrectly adding in the offset
of the area that is to be counted. Remove the incorrect x offset from
the calculation.

Thanks to the Sikshana Foundation for reporting this issue.

sdaps/image/surface.c
@@ -290,9 +290,9 @@ count_black_pixel_masked_unchecked(guint32* pixels, guint32 stride, guint32 *mas
 		int pos;
 
 #if G_BYTE_ORDER == G_BIG_ENDIAN
-		end_mask = (0xffffffff << (-(x + width) & 0x1f));
+		end_mask = (0xffffffff << (-width & 0x1f));
 #else
-		end_mask = (0xffffffff >> (-(x + width) & 0x1f));
+		end_mask = (0xffffffff >> (-width & 0x1f));
 #endif
 		end = (width - 1) >> 5;
 